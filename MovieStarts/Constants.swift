eae26158ed527889538e44e82347e6e48028c147
Oliver Eichhorn <oli@Olivers-iMac.local>
2015-06-20 15:31:58 +0200

    New constants for posters.

--- MovieStarts/Constants.swift
+++ MovieStarts/Constants.swift
@@ -13,8 +13,8 @@ struct Constants {
 	
 	static let CLOUDKIT_CONTAINER_ID		= "iCloud.com.icorn.MovieStarts"
 	static let MOVIESTARTS_GROUP			= "group.com.icorn.MovieStarts"
-
-//	static let DOCUMENTS_FOLDER				= NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+	static let THUMBNAIL_FOLDER				= "/Thumbnails"
+	static let BIG_POSTER_FOLDER			= "/BigPoster"
 	
 	static let PREFS_LATEST_DB_MODIFICATION = "modificationDate"
 	static let PREFS_LATEST_DB_UPDATE_CHECK = "lastestCheck"
@@ -36,11 +36,8 @@ struct Constants {
 	
 	static let DB_ID_TMDB_ID				= "tmdbId"
 	static let DB_ID_ORIG_TITLE				= "origTitle"
-//	static let DB_ID_POPULARITY				= "popularity"
 	static let DB_ID_RUNTIME				= "runtime"
 	static let DB_ID_VOTE_AVERAGE			= "voteAverage"
-//	static let DB_ID_VOTE_COUNT				= "voteCount"
-	
 	static let DB_ID_TITLE					= "title"
 	static let DB_ID_SYNOPSIS				= "synopsis"
 	static let DB_ID_RELEASE				= "release"
@@ -48,15 +45,18 @@ struct Constants {
 	static let DB_ID_CERTIFICATION			= "certification"
 	static let DB_ID_POSTER_URL				= "posterUrl"
 	static let DB_ID_PRODUCTION_COUNTRIES	= "productionCountries"
-	
 	static let DB_ID_IMDB_ID				= "imdbId"
 	static let DB_ID_DIRECTORS				= "directors"
 	static let DB_ID_ACTORS					= "actors"
-	
 	static let DB_ID_TRAILER_NAMES			= "trailerNames"
 	static let DB_ID_TRAILER_IDS			= "trailerIds"
 	static let DB_ID_ASSET					= "asset"
-	
 	static let DB_ID_HIDDEN					= "hidden"
+	
+	static let DB_ID_POSTER_ASSET			= "posterAsset"
+	static let DB_ID_BIG_POSTER_ASSET		= "bigPosterAsset"
+
+//	static let DB_ID_POPULARITY				= "popularity"
+//	static let DB_ID_VOTE_COUNT				= "voteCount"
 
 }
