a508e264314a17986c65aafee62f04bb09f91757
Oliver Eichhorn <oli@Olivers-iMac.local>
2015-07-13 12:57:18 +0200

    Settings icons and titles of tab-bar-controller correctly.

--- MovieStarts/TabBarController.swift
+++ MovieStarts/TabBarController.swift
@@ -15,21 +15,49 @@ class TabBarController: UITabBarController {
 	@IBOutlet weak var movieTabBar: UITabBar!
 	
 
-	override func viewDidLoad() {
-        super.viewDidLoad()
-/*
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+
 		if let saveItems = self.movieTabBar.items {
 			if (saveItems.count == 3) {
+				
+				// set tab bar titles
+				
 				(saveItems[0] as! UITabBarItem).title = NSLocalizedString("NowPlaying", comment: "")
 				(saveItems[1] as! UITabBarItem).title = NSLocalizedString("Upcoming", comment: "")
 				(saveItems[2] as! UITabBarItem).title = NSLocalizedString("Settings", comment: "")
+				
+				// set tab bar images
+
+				(saveItems[0] as! UITabBarItem).image = UIImage(named: "Video.png")
+				(saveItems[1] as! UITabBarItem).image = UIImage(named: "Calendar.png")
+				(saveItems[2] as! UITabBarItem).image = UIImage(named: "Settings.png")
 			}
 		}
-*/
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+	override func supportedInterfaceOrientations() -> Int {
+		
+		if let svc = selectedViewController {
+			return svc.supportedInterfaceOrientations()
+		}
+		else {
+			return Int(UIInterfaceOrientationMask.Portrait.rawValue)
+		}
+	}
+	
+/*
+	override func shouldAutorotate() -> Bool {
+		if let svc = selectedViewController {
+			return svc.shouldAutorotate()
+		}
+		else {
+			return false
+		}
+	}
+*/
 }
