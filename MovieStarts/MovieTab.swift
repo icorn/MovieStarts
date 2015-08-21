9ef1aff7d9af5c56a498e3909c9d33bb23b61c13
Oliver Eichhorn <oli@Olivers-iMac.local>
2015-08-21 19:41:46 +0200

    New file to identify the current tab.

---
+++ MovieStarts/MovieTab.swift
@@ -0,0 +1,16 @@
+//
+//  MovieTab.swift
+//  MovieStarts
+//
+//  Created by Oliver Eichhorn on 21.08.15.
+//  Copyright (c) 2015 Oliver Eichhorn. All rights reserved.
+//
+
+import Foundation
+
+
+enum MovieTab {
+	case NowPlaying
+	case Upcoming
+	case Favorites
+}
