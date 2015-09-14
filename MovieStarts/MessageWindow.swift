dc9a3af6c4880ae087b2d822efb8388863c958d7
Oliver Eichhorn <oli@Olivers-iMac.fritz.box>
2015-09-14 18:24:10 +0200

    Message window (e. g. for error messages) now with auto-layout. Fixes #3.

--- MovieStarts/MessageWindow.swift
+++ MovieStarts/MessageWindow.swift
@@ -9,92 +9,197 @@
 import Foundation
 import UIKit
 
+
 class MessageWindow: NSObject {
 	
-	var backView: UIView?
-	var view: UIView?
+	var backView: UIView
+	var view: UIView
 	var logoImageView: UIImageView?
-	var button: UIButton?
-	var progressLabel: UILabel?
-	var spinner: UIActivityIndicatorView?
+	var button: UIButton
+	var progressView: UIView
+	var progressLabel: UILabel
+	var spinner: UIActivityIndicatorView
+	var progressViewWidthConstraint: NSLayoutConstraint?
+	
 	weak var parentView: UIView?
 	var buttonHandler: (() -> ())?
 	
 	
 	init(parent: UIView, darkenBackground: Bool, titleStringId: String, textStringId: String, buttonStringId: String, handler: (() -> ())?) {
-		
-		super.init()
-		
-		var viewWidth: CGFloat  = 280
-		var viewHeight: CGFloat = 260 // we be overwritten later
-		var bodyInset: CGFloat	= 15
-		
+
 		parentView = parent
 		buttonHandler = handler
 		
-		backView = UIView(frame: parent.frame)
-		view = UIView(frame: CGRect(x: parent.frame.width / 2 - viewWidth / 2, y: parent.frame.height / 2 - viewHeight / 2, width: viewWidth, height: viewHeight))
+		// create views
+
+		view = UIView()
+		backView = UIView()
+		button = UIButton()
+		progressView = UIView()
+		spinner = UIActivityIndicatorView()
+		progressLabel = UILabel()
+
+		super.init()
+
+		// set up views
 		
-		if let view = view, backView = backView {
-			
-			if darkenBackground {
-				backView.backgroundColor = UIColor.blackColor()
-				backView.alpha = 0.7
-			}
-			
-			view.layer.cornerRadius = 6
-			view.backgroundColor = UIColor.whiteColor()
-			view.opaque = false
-			
-			// title view
-			var title = UILabel(frame: CGRect(x: 0, y: 40, width: viewWidth, height: 20))
-			title.text = NSLocalizedString(titleStringId, comment: "")
-			title.font = UIFont.systemFontOfSize(24)
-			title.textAlignment = NSTextAlignment.Center
-			title.textColor = UIColor.blackColor()
-			title.backgroundColor = UIColor.clearColor()
-			view.addSubview(title)
-			
-			// message text
-			var msg = UILabel(frame: CGRect(x: bodyInset, y: title.frame.maxY + 10, width: viewWidth - 2 * bodyInset, height: 150))
-			msg.text = NSLocalizedString(textStringId, comment: "")
-			msg.font = UIFont.systemFontOfSize(16)
-			msg.textAlignment = NSTextAlignment.Center
-			msg.textColor = UIColor.blackColor()
-			msg.backgroundColor = UIColor.clearColor()
-			msg.numberOfLines = 0
-			msg.sizeToFit()
-			view.addSubview(msg)
-			
-			// button
-			button = UIButton(frame: CGRect(x: 0, y: msg.frame.maxY + 20, width: viewWidth, height: 30))
-			
-			if let button = button {
-				button.setTitle(NSLocalizedString(buttonStringId, comment: ""), forState: UIControlState.Normal)
-				button.setTitleColor(UIColor(red: 0.0, green: 170.0/255.0, blue: 170.0/255.0, alpha: 1.0), forState: UIControlState.Normal)
-				button.setTitleColor(UIColor(red: 0.0, green: 120.0/255.0, blue: 120.0/255.0, alpha: 1.0), forState: UIControlState.Highlighted)
-				button.addTarget(self, action: Selector("buttonPressed"), forControlEvents: UIControlEvents.TouchUpInside)
-				view.addSubview(button)
-				
-				// resize the view depending on the height of the content
-				view.frame = CGRect(x: view.frame.minX, y: view.frame.minY, width: viewWidth, height: button.frame.maxY + 20)
-			}
-			
-			// the nice logo
-			var logoImage = UIImage(named: "welcome")
-			if let logoImage = logoImage {
-				logoImageView = UIImageView(frame: CGRect(x: parent.frame.width / 2 - logoImage.size.width / 2, y: view.frame.minY - logoImage.size.height / 2,
-					width: logoImage.size.width, height: logoImage.size.height))
-				logoImageView?.image = logoImage
-			}
-			
-			parent.addSubview(backView)
-			parent.addSubview(view)
-			
-			if let logoImageView = logoImageView {
-				parent.addSubview(logoImageView)
-			}
+		view.setTranslatesAutoresizingMaskIntoConstraints(false)
+		view.layer.cornerRadius = 6
+		view.backgroundColor = UIColor.whiteColor()
+		view.opaque = false
+		
+		backView.setTranslatesAutoresizingMaskIntoConstraints(false)
+		if darkenBackground {
+			backView.backgroundColor = UIColor.blackColor()
+			backView.alpha = 0.7
 		}
+		
+		var logoImage = UIImage(named: "welcome")
+		if let logoImage = logoImage {
+			logoImageView = UIImageView()
+			logoImageView?.setTranslatesAutoresizingMaskIntoConstraints(false)
+			logoImageView?.image = logoImage
+		}
+		
+		var title = UILabel()
+		title.setTranslatesAutoresizingMaskIntoConstraints(false)
+		title.text = NSLocalizedString(titleStringId, comment: "")
+		title.font = UIFont.systemFontOfSize(24)
+		title.textAlignment = NSTextAlignment.Center
+		title.textColor = UIColor.blackColor()
+		title.backgroundColor = UIColor.clearColor()
+
+		var msg = UILabel()
+		msg.setTranslatesAutoresizingMaskIntoConstraints(false)
+		msg.text = NSLocalizedString(textStringId, comment: "")
+		msg.font = UIFont.systemFontOfSize(16)
+		msg.textAlignment = NSTextAlignment.Center
+		msg.textColor = UIColor.blackColor()
+		msg.backgroundColor = UIColor.clearColor()
+		msg.numberOfLines = 0
+		msg.sizeToFit()
+		
+		button.setTranslatesAutoresizingMaskIntoConstraints(false)
+		button.setTitle(NSLocalizedString(buttonStringId, comment: ""), forState: UIControlState.Normal)
+		button.setTitleColor(UIColor(red: 0.0, green: 170.0/255.0, blue: 170.0/255.0, alpha: 1.0), forState: UIControlState.Normal)
+		button.setTitleColor(UIColor(red: 0.0, green: 120.0/255.0, blue: 120.0/255.0, alpha: 1.0), forState: UIControlState.Highlighted)
+		button.addTarget(self, action: Selector("buttonPressed"), forControlEvents: UIControlEvents.TouchUpInside)
+		
+		progressView.setTranslatesAutoresizingMaskIntoConstraints(false)
+		progressView.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize)
+		progressView.hidden = true
+		
+		spinner.setTranslatesAutoresizingMaskIntoConstraints(false)
+		spinner.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+		spinner.hidesWhenStopped = false
+		
+		progressLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+		progressLabel.text = "Hi"
+		progressLabel.font = UIFont.systemFontOfSize(16)
+		progressLabel.textAlignment = NSTextAlignment.Left
+		progressLabel.textColor = UIColor.grayColor()
+		progressLabel.backgroundColor = UIColor.clearColor()
+		progressLabel.sizeToFit()
+		
+		// add views to parents
+		
+		parent.addSubview(backView)
+		parent.addSubview(view)
+		
+		view.addSubview(title)
+		view.addSubview(msg)
+		view.addSubview(button)
+		view.addSubview(progressView)
+
+		progressView.addSubview(spinner)
+		progressView.addSubview(progressLabel)
+
+		if let logoImageView = logoImageView {
+			parent.addSubview(logoImageView)
+		}
+		
+		// create easy constraints in visual format
+		
+		var viewsDictionary = ["view": view, "backView": backView, "progressView": progressView]
+		
+		parent.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[backView]-0-|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary))
+		parent.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[backView]-0-|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary))
+		parent.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-20-[view]-20-|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary))
+		
+		// create the more complicated constraints in code
+
+		if let logoImageView = logoImageView, logoImage = logoImage {
+			parent.addConstraints([
+				NSLayoutConstraint(item: logoImageView,	attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: view,
+					attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: -1 * (logoImage.size.height / 2)),
+				NSLayoutConstraint(item: logoImageView, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: view,
+					attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0)
+			])
+		}
+		
+		view.addConstraints([
+			NSLayoutConstraint(item: title, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: view,
+				attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 40),
+			NSLayoutConstraint(item: title, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: view,
+				attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0),
+			NSLayoutConstraint(item: title, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: view,
+				attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 0)
+		])
+				
+		view.addConstraints([
+			NSLayoutConstraint(item: msg, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: title,
+				attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 10),
+			NSLayoutConstraint(item: msg, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: view,
+				attribute: NSLayoutAttribute.Leading, multiplier: 1.0, 	constant: 15),
+			NSLayoutConstraint(item: msg, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: view,
+				attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: -15)
+		])
+
+		view.addConstraints([
+			NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: view,
+				attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0),
+			NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: view,
+				attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 0),
+			NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: msg,
+				attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 20)
+		])
+		
+		progressView.addConstraints([
+			NSLayoutConstraint(item: spinner, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: progressView,
+				attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0),
+			NSLayoutConstraint(item: spinner, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: progressView,
+				attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0),
+			
+			NSLayoutConstraint(item: progressLabel, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: spinner,
+				attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 10),
+			NSLayoutConstraint(item: progressLabel, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: progressView,
+				attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0)
+		])
+		
+		view.addConstraints([
+			NSLayoutConstraint(item: progressView, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: view,
+				attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0),
+			NSLayoutConstraint(item: progressView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: button,
+				attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0),
+
+			NSLayoutConstraint(item: progressView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: button,
+				attribute: NSLayoutAttribute.Height, multiplier: 1.0, constant: 0),
+
+		])
+
+		progressViewWidthConstraint =  NSLayoutConstraint(item: progressView, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil,
+			attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: progressLabel.frame.width + 30)
+
+		if let constraint = progressViewWidthConstraint {
+			view.addConstraint(constraint)
+		}
+		
+		parent.addConstraints([
+			NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: parent,
+				attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0),
+			NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: button,
+				attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 20)
+		])
 	}
 	
 	
@@ -105,60 +210,31 @@ class MessageWindow: NSObject {
 	
 	func close() {
 		logoImageView?.removeFromSuperview()
-		button?.removeFromSuperview()
-		view?.removeFromSuperview()
-		backView?.removeFromSuperview()
+		view.removeFromSuperview()
+		backView.removeFromSuperview()
 	}
-
+	
 	
 	func showProgressIndicator(progressText: String) {
-		if let button = button, view = view {
-			button.hidden = true
-			
-			spinner = UIActivityIndicatorView(frame: CGRect(x: 40, y: button.frame.minY + 5, width: 20, height: 20))
-			
-			if let spinner = spinner {
-				spinner.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
-				spinner.startAnimating()
-				view.addSubview(spinner)
-				
-				progressLabel = UILabel(frame: CGRect(x: spinner.frame.maxX + 10, y: spinner.frame.minY, width: 100, height: 30))
-				
-				if let progressLabel = self.progressLabel {
-					progressLabel.text = progressText
-					progressLabel.font = UIFont.systemFontOfSize(16)
-					progressLabel.textAlignment = NSTextAlignment.Left
-					progressLabel.textColor = UIColor.grayColor()
-					progressLabel.backgroundColor = UIColor.clearColor()
-					progressLabel.sizeToFit()
-					view.addSubview(progressLabel)
-					
-					centerProgress()
-				}
-			}
-		}
-	}
+		button.hidden = true
+		spinner.startAnimating()
+		progressView.hidden = false
 
+		updateProgressIndicator(progressText)
+	}
+	
 	
 	func updateProgressIndicator(progressText: String) {
-		if let progressLabel = progressLabel {
-			dispatch_async(dispatch_get_main_queue()) {
-				progressLabel.text = progressText
-				progressLabel.sizeToFit()
-				self.centerProgress()
+		
+		dispatch_async(dispatch_get_main_queue()) {
+			self.progressLabel.text = progressText
+			self.progressLabel.sizeToFit()
+		
+			if let constraint = self.progressViewWidthConstraint {
+				constraint.constant = self.progressLabel.frame.width + 30
 			}
 		}
 	}
 	
-	
-	private func centerProgress() {
-		// center both the spinner and the label
-		if let progressLabel = progressLabel, spinner = spinner, view = view {
-			var progressWidth = 20 + 10 + progressLabel.frame.width
-			var newProgressX = (view.frame.width - progressWidth) / 2
-			spinner.frame = CGRect(x: newProgressX, y: spinner.frame.minY, width: spinner.frame.width, height: spinner.frame.height)
-			progressLabel.frame = CGRect(x: spinner.frame.maxX + 10, y: progressLabel.frame.minY, width: progressLabel.frame.width, height: progressLabel.frame.height)
-		}
-	}
-
 }
+
