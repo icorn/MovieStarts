8c7ce81b15a3ab6520a9c27da454d1f834b7bb91
Oliver Eichhorn <oli@Olivers-iMac.fritz.box>
2015-09-20 19:35:28 +0200

    New parent class for both database classes.

---
+++ MovieStarts/DatabaseParent.swift
@@ -0,0 +1,31 @@
+//
+//  DatabaseParent.swift
+//  MovieStarts
+//
+//  Created by Oliver Eichhorn on 20.09.15.
+//  Copyright (c) 2015 Oliver Eichhorn. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+
+class DatabaseParent {
+	
+	var cloudKitContainer: CKContainer
+	var cloudKitDatabase: CKDatabase
+	var recordType: String
+
+	
+	init(recordType: String) {
+		self.recordType = recordType
+		
+		cloudKitContainer = CKContainer(identifier: Constants.CLOUDKIT_CONTAINER_ID)
+		cloudKitDatabase = cloudKitContainer.publicCloudDatabase
+	}
+
+	
+	func checkCloudKit(handler: (CKAccountStatus, NSError!) -> ()) {
+		cloudKitContainer.accountStatusWithCompletionHandler(handler)
+	}
+}
