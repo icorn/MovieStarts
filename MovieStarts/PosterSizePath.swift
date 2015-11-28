bb2babfaaa0e8d6546c2ef5d69b593cffb7686ba
Oliver Eichhorn <oeichhorn@me.com>
2015-11-28 10:27:46 +0100

    New enum for handling different poster sizes.

---
+++ MovieStarts/PosterSizePath.swift
@@ -0,0 +1,14 @@
+//
+//  PosterSizePath.swift
+//  MovieStarts
+//
+//  Created by Oliver Eichhorn on 24.11.15.
+//  Copyright © 2015 Oliver Eichhorn. All rights reserved.
+//
+
+import Foundation
+
+enum PosterSizePath: String {
+	case Small	= "w92"
+	case Big	= "w500"
+}
