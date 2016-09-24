deba6f9b6ab2cc421259812194d16ae318e89e63
Oliver Eichhorn <oeichhorn@me.com>
2016-09-24 19:26:33 +0200

    Crashlytics update

---
+++ MovieStarts/MovieViewControllerTrailers.swift
@@ -0,0 +1,9 @@
+//
+//  MovieViewControllerTrailers.swift
+//  MovieStarts
+//
+//  Created by Oliver Eichhorn on 23.09.16.
+//  Copyright © 2016 Oliver Eichhorn. All rights reserved.
+//
+
+import Foundation
